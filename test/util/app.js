fc2e9890cfec4c8a04f6934c8103b1b0864e7f01
Sarah <asarahali00@gmail.com>
2021-04-03 15:20:09 -0500

    update connection url in app.js

--- test/util/app.js
+++ test/util/app.js
@@ -40,7 +40,8 @@ function killServer(server) {
 
 async function spawnApp() {
   const env = {
-    MONGODB_CONNECTION_URL: 'mongodb://localhost:27017',
+    MONGODB_CONNECTION_URL: 'mongodb://localhost:27017/test',
+    MONGO_DATABASE: 'test',
     MONGO_REPLICA_SET: 'test',
     PATH: process.env.PATH,
     PORT: await getPort(),
