79d36cc008f3e7fe903501254cdad4a28b185df1
Sarah <asarahali00@gmail.com>
2021-04-03 15:55:04 -0500

    update repl set name

--- test/util/app.js
+++ test/util/app.js
@@ -40,7 +40,7 @@ function killServer(server) {
 
 async function spawnApp() {
   const env = {
-    MONGODB_CONNECTION_URL: 'mongodb://localhost:27017/test,localhost:27017/test?replicaSet=rs1',
+    MONGODB_CONNECTION_URL: 'mongodb://localhost:27017/test,localhost:27017/test?replicaSet=testrs',
     PATH: process.env.PATH,
     PORT: await getPort(),
   };
